bfb51d6b648c0eb9f7b4ed7d22490e0fb541dc8e
Xiangyun Huang <xiangyunfaith@outlook.com>
2023-05-11 13:26:55 +0800

    Markdown 可视化编辑器格式化文档

--- _common.R
+++ _common.R
@@ -20,65 +20,18 @@ if (xfun::is_macos()) {
     regular = "NotoSerifCJKsc-Regular.otf",
     bold = "NotoSerifCJKsc-Bold.otf"
   )
-  ## 黑体
-  sysfonts::font_add(
-    family = "Noto Sans CJK SC",
-    regular = "NotoSansCJKsc-Regular.otf",
-    bold = "NotoSansCJKsc-Bold.otf"
-  )
-} else if (grepl(x = sessionInfo()$running, pattern = "Fedora")) { # Github Action custom Docker Container Based on Fedora
+} else { # Github Action custom Docker Container Based on Fedora
   sysfonts::font_paths(new = c(
     "/usr/share/fonts/google-noto/",
-    "/usr/share/fonts/google-noto-serif-cjk-fonts", # Fedora 38
-    "/usr/share/fonts/google-noto-sans-cjk-fonts"   # Fedora 38
+    "/usr/share/fonts/google-noto-serif-cjk-fonts" # Fedora 38
   ))
   ## 宋体
   sysfonts::font_add(
     family = "Noto Serif CJK SC",
     regular = "NotoSerifCJK-Regular.ttc",
     bold = "NotoSerifCJK-Bold.ttc"
-  )
-  ## 黑体
-  sysfonts::font_add(
-    family = "Noto Sans CJK SC",
-    regular = "NotoSansCJK-Regular.ttc",
-    bold = "NotoSansCJK-Bold.ttc"
-  )
-} else { # Github Action Ubuntu
-  sysfonts::font_paths(new = c(
-    "/usr/share/fonts/opentype/noto/",
-    "/usr/share/fonts/truetype/noto/"
-  ))
-  ## 宋体
-  sysfonts::font_add(
-    family = "Noto Serif CJK SC",
-    regular = "NotoSerifCJK-Regular.ttc",
-    bold = "NotoSerifCJK-Bold.ttc"
-  )
-  ## 黑体
-  sysfonts::font_add(
-    family = "Noto Sans CJK SC",
-    regular = "NotoSansCJK-Regular.ttc",
-    bold = "NotoSansCJK-Bold.ttc"
   )
 }
-
-## 衬线字体
-sysfonts::font_add(
-  family = "Noto Serif",
-  regular = "NotoSerif-Regular.ttf",
-  bold = "NotoSerif-Bold.ttf",
-  italic = "NotoSerif-Italic.ttf",
-  bolditalic = "NotoSerif-BoldItalic.ttf"
-)
-## 无衬线字体
-sysfonts::font_add(
-  family = "Noto Sans",
-  regular = "NotoSans-Regular.ttf",
-  bold = "NotoSans-Bold.ttf",
-  italic = "NotoSans-Italic.ttf",
-  bolditalic = "NotoSans-BoldItalic.ttf"
-)
 
 # 设置 Web GL 渲染
 options(rgl.useNULL = TRUE)
