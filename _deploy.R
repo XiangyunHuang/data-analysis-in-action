ff7109322985a225918fbf60369a4de68ee201dd
XiangyunHuang <xiangyunfaith@outlook.com>
2023-07-09 14:53:41 +0800

    Fix CI

--- _deploy.R
+++ _deploy.R
@@ -1,5 +1,5 @@
 # On CI connect to server, using API KEY and deploy using appId
-rsconnect::addServer(url = "https://bookdown.org", name = "bookdown.org")
+rsconnect::addServer("https://bookdown.org", "bookdown.org")
 rsconnect::connectApiUser(
   account = "xiangyun", server = "bookdown.org",
   apiKey = Sys.getenv("CONNECT_API_KEY")
