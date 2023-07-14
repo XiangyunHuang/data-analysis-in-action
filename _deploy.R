404103d11c102d93ebb29f69eb8520cd2f8057d8
XiangyunHuang <xiangyunfaith@outlook.com>
2023-07-14 11:55:24 +0800

    Bookdown 部署 API 有变动

--- _deploy.R
+++ _deploy.R
@@ -1,5 +1,5 @@
 # On CI connect to server, using API KEY and deploy using appId
-rsconnect::addServer("https://bookdown.org", "bookdown.org")
+rsconnect::addServer("https://bookdown.org/__api__/", "bookdown.org")
 rsconnect::connectApiUser(
   account = "xiangyun", server = "bookdown.org",
   apiKey = Sys.getenv("CONNECT_API_KEY")
