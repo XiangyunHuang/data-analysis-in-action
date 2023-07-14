ec42d80c6c6856288c8cf39d8e8471ca124b2b8a
XiangyunHuang <xiangyunfaith@outlook.com>
2023-07-14 12:53:10 +0800

    name 可以任意取

--- _deploy.R
+++ _deploy.R
@@ -1,12 +1,12 @@
 # On CI connect to server, using API KEY and deploy using appId
-rsconnect::addServer(url = "https://bookdown.org/", name = "bookdown.org")
+rsconnect::addServer(url = "https://bookdown.org/", name = "quarto")
 rsconnect::connectApiUser(
-  account = "xiangyun", server = "bookdown.org",
+  account = "xiangyun", server = "quarto",
   apiKey = Sys.getenv("CONNECT_API_KEY")
 )
-rsconnect::addServer(url = "https://bookdown.org/__api__/", name = "bookdown.org")
+rsconnect::addServer(url = "https://bookdown.org/__api__/", name = "quarto")
 quarto::quarto_publish_site(
   name = "data-analysis-in-action", render = "none",
-  server = "bookdown.org", account = "xiangyun",
+  server = "quarto", account = "xiangyun",
   title = "Data Analysis in Action"
 )
