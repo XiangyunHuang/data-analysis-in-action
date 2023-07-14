150d4873ce33744ddd132990ea0141084ef427e0
XiangyunHuang <xiangyunfaith@outlook.com>
2023-07-14 12:11:07 +0800

    Fedora 上不需要加后缀 /__api__/

--- _deploy.R
+++ _deploy.R
@@ -1,12 +1,11 @@
 # On CI connect to server, using API KEY and deploy using appId
-rsconnect::addServer("https://bookdown.org/__api__/", "bookdown.org")
+rsconnect::addServer(url = "https://bookdown.org/", name = "bookdown.org")
 rsconnect::connectApiUser(
   account = "xiangyun", server = "bookdown.org",
   apiKey = Sys.getenv("CONNECT_API_KEY")
 )
 quarto::quarto_publish_site(
   name = "data-analysis-in-action", render = "none",
-  server = "bookdown.org",
-  account = "xiangyun",
+  server = "bookdown.org", account = "xiangyun",
   title = "Data Analysis in Action"
 )
