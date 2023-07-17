afa1a9b424cdd36417f20dc806a83ed3bb0df2b5
Xiangyun Huang <xiangyunfaith@outlook.com>
2023-07-17 23:17:55 +0800

    PDF 文档 (#88)
    * `filter()` 函数对数据的平滑作用
    
    * Bookdown 部署 API 有变动
    
    * Fedora 上不需要加后缀 /__api__/
    
    * another try
    
    * name 可以任意取
    
    * [skip ci] 主分支部署到 bookdown.org
    
    * 时间序列分解
    
    * dygraphs 包绘制动态时间序列数据
    
    * 更新美团股价数据
    
    * 添加静态图片
    
    * tweak
    
    * try to use rsconnect instead of quarto
    
    * LaTeX 文档
    
    * tweak
    
    * tweak
    
    * tweak
    
    * 忘了上传图片
    
    * tweak
    
    * 尝试编译 PDF 文档
    
    * tweak
    
    * Quarto 引用
    
    * Quarto 基础
    
    * Qmd 文件中插入 Qmd 源文件
    
    * R Markdown 入门
    
    * align
    
    * 修复交叉引用
    
    * fix ci
    
    * Fix

--- _deploy.R
+++ _deploy.R
@@ -1,12 +1,24 @@
 # On CI connect to server, using API KEY and deploy using appId
-rsconnect::addServer("https://bookdown.org", "bookdown.org")
+rsconnect::addServer(url = "https://bookdown.org/", name = "quarto")
 rsconnect::connectApiUser(
-  account = "xiangyun", server = "bookdown.org",
+  account = "xiangyun", server = "quarto",
   apiKey = Sys.getenv("CONNECT_API_KEY")
 )
-quarto::quarto_publish_site(
-  name = "data-analysis-in-action", render = "none",
-  server = "bookdown.org",
-  account = "xiangyun",
-  title = "Data Analysis in Action"
+## TODO: rsconnect 1.0.0 发布后使用 deploySite 部署
+# rsconnect::deploySite(
+#   siteName = "data-analysis-in-action",
+#   siteTitle = "Data Analysis in Action",
+#   server = "quarto", account = "xiangyun",
+#   render = "none"
+# )
+## TODO: rsconnect 1.0.0 发布后去掉下面的部署代码
+rsconnect::addServer(url = "https://bookdown.org/__api__/", name = "quarto")
+rsconnect::deployApp(
+  appDir = "_book",
+  appId = Sys.getenv("CONTENT_ID"),
+  contentCategory = "site",
+  appName = "data-analysis-in-action",
+  appTitle = "Data Analysis in Action",
+  server = "quarto", account = "xiangyun",
+  forceUpdate = TRUE
 )
