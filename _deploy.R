4d1cc0af350170eaf77c953d3aa9e04f7ec9642b
XiangyunHuang <xiangyunfaith@outlook.com>
2023-07-17 14:58:45 +0800

    tweak

--- _deploy.R
+++ _deploy.R
@@ -4,6 +4,14 @@ rsconnect::connectApiUser(
   account = "xiangyun", server = "quarto",
   apiKey = Sys.getenv("CONNECT_API_KEY")
 )
+## TODO: rsconnect 1.0.0 发布后使用 deploySite 部署
+# rsconnect::deploySite(
+#   siteName = "data-analysis-in-action",
+#   siteTitle = "Data Analysis in Action",
+#   server = "quarto", account = "xiangyun",
+#   render = "none"
+# )
+## TODO: rsconnect 1.0.0 发布后去掉下面的部署代码
 rsconnect::addServer(url = "https://bookdown.org/__api__/", name = "quarto")
 rsconnect::deployApp(
   appDir = "_book",
