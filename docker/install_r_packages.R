4b68dc7df22fcc81cdc8d251af16a7b5ba6e934c
XiangyunHuang <xiangyunfaith@outlook.com>
2023-12-09 20:44:50 +0800

    [docker] INLA 必须最后安装

--- docker/install_r_packages.R
+++ docker/install_r_packages.R
@@ -5,9 +5,9 @@ desc_df <- desc::desc_get_deps()
 pkgs <- desc_df[desc_df$type != "Depends", "package"]
 
 install.packages(pkgs = setdiff(pkgs, c("INLA", "cmdstanr", "plot2", "rjags")))
-install.packages("INLA", repos = c(getOption("repos"), INLA = "https://inla.r-inla-download.org/R/stable"))
 install.packages("cmdstanr", repos = c(getOption("repos"), STAN = "https://mc-stan.org/r-packages/"))
 install.packages("plot2", repos = c(getOption("repos"), PLOT = "https://grantmcdermott.r-universe.dev"))
 remotes::install_github(c("davidsjoberg/ggbump", "davidsjoberg/ggstream"))
-
+# INLA 包含一些旧的动态链接库，最好放最后安装
+install.packages("INLA", repos = c(getOption("repos"), INLA = "https://inla.r-inla-download.org/R/stable"))
 # rjags and rstanarm can't be installed because Rocky Linux 9 doesn't have JAGS and Posit doesn't have rstanarm 
